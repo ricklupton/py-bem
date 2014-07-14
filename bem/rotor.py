e8247b7266ba69ad2956cd534e4a980eb3f74b3b
Rick Lupton <r.lupton@gmail.com>
2014-07-14 11:33:20 +0100

    Methods to calculate rotor loads

--- bem/rotor.py
+++ bem/rotor.py
@@ -4,7 +4,8 @@ Rotor model using mbwind
 
 import numpy as np
 from numpy import pi, dot
-from mbwind import rotations, RigidConnection, RigidBody, Hinge
+from mbwind import RigidConnection, RigidBody, Hinge
+from mbwind.utils import rotations
 from mbwind.elements.modal import ModalElementFromFE
 
 
@@ -23,7 +24,7 @@ class Rotor(object):
         self.blades = []
         self.pitch_bearings = []
         for ib in range(num_blades):
-            R = rotations(('y', -pi/2), ('x', ib*2*pi/3))
+            R = rotations(('x', ib*2*pi/3), ('y', -pi/2))
             root_offset = dot(R, [root_length, 0, 0])
             root = RigidConnection('root%d' % (ib+1), root_offset, R)
             blade = ModalElementFromFE('blade%d' % (ib+1), blade_fe)
@@ -54,3 +55,25 @@ class Rotor(object):
     def transform_aero_to_blade(self, y):
         A = rotations(('y', pi/2))
         return transform_rows(A, y)
+
+    def root_forces_from_blade_loading(self, loading):
+        """loading is [out-of-plane, in-plane] force per unit length on blade"""
+        # Convert to blade proximal coordiantes (x along axis)
+        L = np.zeros((loading.shape[0], 3), dtype=loading.dtype)
+        L[:, 1] = loading[:, 1]
+        L[:, 2] = -loading[:, 0]
+        forces, stress = self.blades[0].calc_forces_from_distributed_loading(L)
+        return forces
+
+    def hub_forces_from_blade_loading(self, loading):
+        forces = self.root_forces_from_blade_loading(loading)
+        # Transform to hub loads (assuming all blades identical)
+        hub = np.array([
+            -forces[2],
+            0,
+            0,
+            -forces[5] - self.root_length * forces[1],
+            0,
+            0,
+        ])
+        return self.num_blades * hub
