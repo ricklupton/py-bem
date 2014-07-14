ccec513cd507c1cbfc902a8a35b913b177635c55
Rick Lupton <r.lupton@gmail.com>
2014-07-14 11:47:31 +0100

    Add new regression test with change in wind speed

--- tests/regression/run-bem.py
+++ tests/regression/run-bem.py
@@ -29,11 +29,15 @@ def build_model(blade_filename, aerofoil_filename):
         # rotorspeed = 22 * (pi/30)  # rad/s
 
 
-def find_solution(model, windspeed, rotorspeed, pitch):
+def find_solution(model, windspeed, rotorspeed, pitch, windspeed2=None):
+    # Calculate forces and derivatives at different windspeed?
+    if windspeed2 is None:
+        windspeed2 = windspeed
+
     # Along blade
     factors = model.solve(windspeed, rotorspeed, pitch)
-    forces = model.forces(windspeed, rotorspeed, pitch, 1.225, factors)
-    xdot = model.inflow_derivatives(windspeed, rotorspeed, pitch,
+    forces = model.forces(windspeed2, rotorspeed, pitch, 1.225, factors)
+    xdot = model.inflow_derivatives(windspeed2, rotorspeed, pitch,
                                     factors)
 
     assert len(model.radii) == len(factors) == len(forces) == len(xdot)
@@ -43,7 +47,7 @@ def find_solution(model, windspeed, rotorspeed, pitch):
         print("\t".join(["%g" % xx for xx in row]))
 
     # Overall rotor
-    pcoeffs = model.pcoeffs(windspeed, rotorspeed, pitch)
+    pcoeffs = model.pcoeffs(windspeed2, rotorspeed, pitch)
     print()
     print("Pcoeffs:")
     print()
@@ -56,4 +60,4 @@ with open(sys.argv[1], 'rt') as f:
 
 model = build_model(inp['blade'], inp['aerofoil'])
 find_solution(model, inp['windspeed'], pi/30 * inp['rotorspeed'],
-              pi/180 * inp['pitch'])
+              pi/180 * inp['pitch'], inp.get('windspeed2', None))
